de08ad3074b23c568afbdd76fd55b2b0492775b8
Alexander <smlwmy@gmail.com>
2018-07-07 17:55:49 +0500

    Урок 5. Подключили jQuery

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  '/javascripts',
+    express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+);
+
 
 app.get('/', (req, res) => {
     Post.find({}).then(posts => {
